a07df40a00f80d5364163d933774cbfc043fd6d1
yuemingliu <yuemingliu@tencent.com>
2023-12-07 19:36:16 +0800

    基础功能

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+module.exports = {
+  create(context) {
+    return {
+      Literal(node) {
+        if (typeof node.value === 'string') {
+          const regex = /([\u4e00-\u9fff](?=[a-zA-Z]))|([a-zA-Z](?=[\u4e00-\u9fff]))/g;
+          const matches = [];
+          let match;
+          while ((match = regex.exec(node.value)) !== null) {
+            matches.push(match);
+          }
+          if (matches.length) {
+            context.report({
+              node,
+              message: `在中英文之间应插入空格: ${node.value}`,
+              fix: fixer => fixer.replaceText(node, node.value.replace(regex, '$1$2 '))
+            })
+          }
+        }
+      },
+    };
+  },
+};
